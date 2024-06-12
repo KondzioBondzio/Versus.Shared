bd4a07c3b7adf43b7516e1b3bfe85efb53661c33
Kamil Domagała <kamilos3312@gmail.com>
2024-06-12 18:12:45 +0200

    Added ConflictResponse dto

---
+++ src/Versus.Shared/Common/ConflictResponse.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace Versus.Shared.Common;
+
+public record ConflictResponse
+{
+    public ConflictResponse(string details)
+    {
+        Details = details;
+    }
+
+    public ConflictResponse(string details, IEnumerable<ConflictDetail> errors)
+    {
+        Details = details;
+        Errors = errors;
+    }
+
+    public string Details { get; init; }
+    public IEnumerable<ConflictDetail> Errors { get; init; } = [];
+}
+
+public record ConflictDetail(string ResourceType, string ResourceIds, string Description)
+{
+    public static ConflictDetail FromResource<TResourceType, TResourceId>(TResourceId resourceId, string description)
+        where TResourceType : class
+    {
+        return new ConflictDetail(typeof(TResourceType).Name, resourceId!.ToString()!, description);
+    }
+
+    public static ConflictDetail FromResource<TResourceType>(IEnumerable resourceIds, string description)
+        where TResourceType : class
+    {
+        return FromResource<TResourceType, string>(string.Join(",", resourceIds), description);
+    }
+}
